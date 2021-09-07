0c7fb278131b647a2a2734fd943793faf0306492
jiange <1563610087@qq.com>
2021-09-07 19:48:31 +0800

    配置css全局变量

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,33 +1,38 @@
 import React from "react";
-import './index.scss'
-import { NavLink } from 'react-router-dom';
-import { headerLinks } from '@/common/js/commonData.js'
-import { SearchOutlined } from '@ant-design/icons';
-import { Input } from 'antd';
+import "./index.scss";
+import { NavLink } from "react-router-dom";
+import { headerLinks } from "@/common/js/commonData.js";
+import { SearchOutlined } from "@ant-design/icons";
+import { Input } from "antd";
 class Header extends React.Component {
-
-	render() {
-		return (
-			<header className='header'>
-				<div className='content'>
-					<div className='header-l'>
-						<div className='logo'></div>
-						<ul>
-							{
-								headerLinks.map((item, index) => {
-									return <li key={index}><NavLink to={item.link} >{item.title}</NavLink></li>
-								})
-							}
-						</ul>
-					</div>
-					<div className='header-r'>
-						<Input size="large" placeholder="音乐/视频" prefix={<SearchOutlined />} className='search'/>
-					</div>
-				</div>
-			</header>
-		)
-	}
-
+  render() {
+    return (
+      <header className="header">
+        <div className="content">
+          <div className="header-l">
+            <div className="logo"></div>
+            <ul>
+              {headerLinks.map((item, index) => {
+                return (
+                  <li key={index}>
+                    <NavLink to={item.link}>{item.title}</NavLink>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+          <div className="header-r">
+            <Input
+              size="large"
+              placeholder="音乐/视频"
+              prefix={<SearchOutlined />}
+              className="search"
+            />
+          </div>
+        </div>
+      </header>
+    );
+  }
 }
 
-export default Header
+export default Header;
